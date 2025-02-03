2ae6c067c4c02c2267303ceaf0de893bd26cb14e
S. M. Mohiuddin Khan Shiam <147746955+mohiuddin-khan-shiam@users.noreply.github.com>
2025-02-03 23:05:38 +0600

    Create Comments.java

---
+++ Basics/Comments.java
@@ -0,0 +1,14 @@
+// Basics/Comments.java
+
+public class Comments {
+    public static void main(String[] args) {
+        // This is a single-line comment
+        System.out.println("Single-line comment above this line.");
+
+        /*
+         * This is a multi-line comment.
+         * It can span multiple lines.
+         */
+        System.out.println("Multi-line comment above this line.");
+    }
+}
