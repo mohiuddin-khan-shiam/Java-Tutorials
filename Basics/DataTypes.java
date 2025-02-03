4a782d60672a769ad1b2cd18a50883753619599f
S. M. Mohiuddin Khan Shiam <147746955+mohiuddin-khan-shiam@users.noreply.github.com>
2025-02-03 23:05:03 +0600

    Create DataTypes.java

---
+++ Basics/DataTypes.java
@@ -0,0 +1,25 @@
+// Basics/DataTypes.java
+
+public class DataTypes {
+    public static void main(String[] args) {
+        // Different data types in Java
+        byte b = 100;
+        short s = 10000;
+        int i = 100000;
+        long l = 100000L;
+        float f = 234.5f;
+        double d = 123.4;
+        boolean bool = true;
+        char c = 'A';
+
+        // Printing data types
+        System.out.println("Byte: " + b);
+        System.out.println("Short: " + s);
+        System.out.println("Int: " + i);
+        System.out.println("Long: " + l);
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+        System.out.println("Boolean: " + bool);
+        System.out.println("Char: " + c);
+    }
+}
