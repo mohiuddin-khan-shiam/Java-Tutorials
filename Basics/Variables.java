bc6099da3d1521d7b6b8cfc88ccac4b75dd4462c
S. M. Mohiuddin Khan Shiam <147746955+mohiuddin-khan-shiam@users.noreply.github.com>
2025-02-03 23:03:45 +0600

    Create Variables.java

---
+++ Basics/Variables.java
@@ -0,0 +1,15 @@
+// Basics/Variables.java
+
+public class Variables {
+    public static void main(String[] args) {
+        // Declaring and initializing variables
+        int number = 10;
+        String text = "Java is fun!";
+        double pi = 3.14159;
+
+        // Printing variables
+        System.out.println("Number: " + number);
+        System.out.println("Text: " + text);
+        System.out.println("Value of Pi: " + pi);
+    }
+}
